bf51146c188f36653ddfce7d3738ec9162ee9e8b
RUSHIKESH MURAHARISETTY <97795983+zenyuzien@users.noreply.github.com>
2025-02-08 01:31:15 +0530

    Add files via upload

---
+++ src/Experiments/myLab.h
@@ -0,0 +1,15 @@
+#ifndef myLab_
+#define myLab_
+
+#include "../structures/structures.h"
+#include "../gates/gates.h"
+Experiment quantum_teleportation_prtcl_var1(mimiqHandler* handler);
+Experiment quantum_teleportation_prtcl_var2(mimiqHandler* handler);
+Experiment quantum_superdense_random(mimiqHandler* handler);
+Experiment quantum_superdense(mimiqHandler* handler);
+Experiment BB98_QKD(mimiqHandler* handler);
+Experiment Grover(mimiqHandler* handler);
+Experiment trial(mimiqHandler* handler);
+Experiment Grover2(mimiqHandler* handler);
+
+#endif
